8fc6b322c6ed6f1b24a7625861512923756d5216
Braian <braian.villsanti2@gmail.com>
2020-05-09 13:05:50 -0300

    ej4

---
+++ Lab04/Empleado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    class Empleado
+    {
+        private int id;
+        private string nombre;
+        private float sueldo;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+        public float Sueldo
+        {
+            get => sueldo;
+            set => sueldo = value;
+        }
+        
+        public Empleado(int id)
+        {
+            Id = id;
+        }
+
+        public void MostrarEmpleado()
+        {
+            Console.WriteLine("{0}\t${1}\t\t{2}", Id, Sueldo, Nombre);
+        }
+    }
+}
---
+++ Lab04/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Empleado> empleados = new List<Empleado>();
+
+            // Carga
+            Console.WriteLine("CARGAR EMPLEADO");
+            int id;
+            do
+            {
+                Console.Write("ID (-1 para terminar): ");
+                id = int.Parse(Console.ReadLine());
+                if (id != -1)
+                {
+                    Empleado e = new Empleado(id);
+                    Console.Write("Nombre: ");
+                    e.Nombre = Console.ReadLine();
+                    Console.Write("Sueldo: $");
+                    e.Sueldo = float.Parse(Console.ReadLine());
+
+                    empleados.Add(e);
+                }
+            } while (id != -1);
+
+            // Ordena
+            var empleadosOrdenados = from e in empleados
+                                     orderby e.Sueldo
+                                     select e;
+
+            // Muestra
+            Console.Clear();
+            Console.WriteLine("ID\tSUELDO\t\tNOMBRE");
+            foreach (Empleado e in empleadosOrdenados)
+            {
+                e.MostrarEmpleado();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
